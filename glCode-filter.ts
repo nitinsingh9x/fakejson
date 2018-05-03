0ccf082581873562b15c5209cb3d4df32d4a553a
Nitin Singh <singhnitins311@gmail.com>
2018-05-03 23:24:33 +0530

    updated

--- glCode-filter.ts
+++ glCode-filter.ts
@@ -1,41 +1,4 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-import { Book } from './book';
-
-@Pipe({
-    name: 'glcodefilter',
-    pure: false
-})
-export class BookFilterPipe implements PipeTransform {
-  transform(items: Book[], filter: Book): Book[] {
-    if (!items || !filter) {
-      return items;
-    }
-    // filter items array, items which match and return true will be kept, false will be filtered out
-    return items.filter((item: Book) => this.applyFilter(item, filter));
+changeFilter(data){
+    this.exclusiveGlCodeList = this.exclusiveGlCodeAll;;
+    this.exclusiveGlCodeList  = this.exclusiveGlCodeList.filter(x=>x.glCd.toLowerCase().indexOf(data.toLowerCase()) === 0);
   }
-  
-  /**
-   * Perform the filtering.
-   * 
-   * @param {Book} book The book to compare to the filter.
-   * @param {Book} filter The filter to apply.
-   * @return {boolean} True if book satisfies filters, false if not.
-   */
-  applyFilter(book: Book, filter: Book): boolean {
-    for (let field in filter) {
-      if (filter[field]) {
-        if (typeof filter[field] === 'string') {
-          if (book[field].toLowerCase().indexOf(filter[field].toLowerCase()) === -1) {
-            return false;
-          }
-        } else if (typeof filter[field] === 'number') {
-          if (book[field] !== filter[field]) {
-            return false;
-          }
-        }
-      }
-    }
-    return true;
-  }
-}
